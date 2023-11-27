e09bc6ca8fb5bdc8dfa773eaf321b6464ca2cce0
DTIC <dtic@Air-de-DTIC.mshome.net>
2023-11-27 09:45:48 +0100

    Primeira parte do Projecto

---
+++ src/main/java/com/amapp/amapp/AmappApplication.java
@@ -0,0 +1,13 @@
+package com.amapp.amapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amapp/amapp/AmappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amapp.amapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
